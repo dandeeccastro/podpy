03ed90f0d85e9966b0e519a7ceff72444fc1af96
Danilo <dundeepessoal@gmail.com>
2020-05-25 17:33:10 -0300

    File path is now working for relative paths

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pod import Main
 import sys
 from optparse import OptionParser
+import os
 
 def main():
 
@@ -11,14 +12,13 @@ def main():
 	parser.add_option("-t","--transition-time",type="int",dest="transition_time",default=2000,help="transition time between chapters in millisecconds [default=2000]")
 	parser.add_option("-s","--song-gain",type="float",dest="music_gain",default=-20.0,help="sound gain in decibels for music mix [default= -20db]")
 	parser.add_option("-f","--filename",type="string",dest="filename",default="podcast.mp3",help="final filename for project")
-	parser.add_option("-m","--music-folder",type="string",dest="music_folder",default="~/Music/",help="music folder for generating the music mix [default= ~/Music]")
+	parser.add_option("-m","--music-folder",type="string",dest="music_folder",default="~/Music/",help="music folder for generating the music mix [default= Music]")
 	parser.add_option("-a","--audio-gain",type="float",dest="voice_gain",default=0,help="sound gain in decibels for voice audio [default = 0db]")
 
 	(options,args) = parser.parse_args()
 
 	if(len(args) > 0):
-		print(args)
-		Main(args[0],options.filename,options.music_folder,options.music_gain,options.transition_time,options.voice_gain).main()
+		Main(os.path.abspath(args[0]),options.filename,os.path.abspath(options.music_folder),options.music_gain,options.transition_time,options.voice_gain).main()
 	else:
 		print("No project folder specified")
 		
